629fc6a2e97639ae06dbaa7c4437f2ae9fa54a7c
Ankita Chhabra <197097691+ankitachhabra29@users.noreply.github.com>
2025-02-15 03:12:11 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+    char grades;
+    scanf("%c",&grades);
+
+    if(grade=='A'){
+        ptintf("Excellent");
+    }
+    if(grade=='B'){
+        ptintf("Good");
+    }
+    if(grade=='C'){
+        ptintf("Average");
+    }
+    if(grade=='D'){
+        ptintf("Below Average");
+    }
+    if(grade=='F'){
+        ptintf("Fail");
+    }
+    
+    return 0;
+}
